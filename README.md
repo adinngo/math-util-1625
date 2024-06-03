f7ff4f437a7968465d41c51303ba9c4f75c1cd9c
adinngo <gnahtong123@gmail.com>
2024-06-03 15:11:04 +0700

    refactor the main code and green 4ever

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 
 ###You will find in this repo the following stuff:
 
-*The Math-util library that offers multiple math functions(a Java-based source code)
+* The Math-util library that offers multiple math functions(a Java-based source code)
 
-*Unit Testing source code with JUnit framework
+* Unit Testing source code with JUnit framework
 
-*....
+* GitHub Action demonstrate the Continous Integartion (CI) technique
+* Regression Test
+* ...
 #### Screenshots
 ![DDT&TDD with JUnit](https://github.com/adinngo/math-util-1625/blob/main/images/DDT%20with%20JUnit.png)
 
--- src/com/ngovinhthang/mathutil/core/MathUtility.java
+++ src/com/ngovinhthang/mathutil/core/MathUtility.java
@@ -20,18 +20,28 @@ public class MathUtility {
     //20 giai thừa 18 con số 0, vừa đủ kiểu long trong java
     //21 giai thừa tràn
     //quy ước chỉ 20!
+//    public static long getFractorial(int n) {
+//        if (n < 0 || n > 20) 
+//            throw new IllegalArgumentException("Invalid agrument. n must be 0...20");
+//
+//        if(n == 0 || n ==1) 
+//            return 1;
+//        
+//        long product = 1;
+//        for(int i = 2; i <= n; i++)
+//            product *= i;
+//        
+//        return product;
+//    }
     public static long getFractorial(int n) {
-        if (n < 0 || n > 20) 
+        if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid agrument. n must be 0...20");
+        }
 
-        if(n == 0 || n ==1) 
+        if (n == 0 || n == 1) {
             return 1;
-        
-        long product = 1;
-        for(int i = 2; i <= n; i++)
-            product *= i;
-        
-        return product;
-    }
+        }
 
+        return n * getFractorial(n - 1);
+    }
 }
